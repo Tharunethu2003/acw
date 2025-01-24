c10fe806a2e274a68cb7f4eff8e398be868b69a4
Tharunethu De Silva <143161333+Tharunethu2003@users.noreply.github.com>
2025-01-24 15:51:34 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    mode: 'jit', // JIT mode should be placed here
+
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './resources/views/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/js/**/*.vue',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                pink: {
+                    50: '#FDF2F8',
+                    100: '#FCE7F3',
+                    200: '#FBCFE8',
+                    300: '#F9A8D4',
+                    400: '#F472B6',
+                    500: '#EC4899',
+                    600: '#DB2777',
+                    700: '#BE185D',
+                    800: '#9D174D',
+                    900: '#831843',
+                },
+            },
+        },
+    },
+
+    plugins: [forms, typography],
+};
